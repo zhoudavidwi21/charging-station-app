b46a2990b8ea0b3c12f185de4eda4a36952000fd
David <david281100@hotmail.com>
2023-05-27 17:13:42 +0200

    finished cleaning up, refactoring

--- Data_Collection_Dispatcher/src/main/java/at/technikum/service/MessagingQueue.java
+++ Data_Collection_Dispatcher/src/main/java/at/technikum/service/MessagingQueue.java
@@ -2,5 +2,6 @@ package at.technikum.service;
 
 public interface MessagingQueue {
 
-    void consumeAndPublish(String inputQueue, String outputQueue1, String outputQueue2) throws Exception;
+    void publish(String queueName, String message) throws Exception;
+    void consume(String queueName) throws Exception;
 }
--- Data_Collection_Dispatcher/src/main/java/at/technikum/service/RabbitMQService.java
+++ Data_Collection_Dispatcher/src/main/java/at/technikum/service/RabbitMQService.java
@@ -6,53 +6,58 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.concurrent.TimeoutException;
 
-public class RabbitMQService implements MessagingQueue {
+public class RabbitMQService implements MessagingQueue, AutoCloseable {
+    private static final String HOST = "localhost";
+    private static final int PORT = 30003;
+    private Connection connection;
+    private Channel channel;
+    private static final String CONTENT_TYPE_JSON = "application/json";
+    private MessageHandler messageHandler;
 
-    ConnectionFactory factory = new ConnectionFactory();
+    public RabbitMQService(MessageHandler messageHandler) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(HOST);
+        factory.setPort(PORT);
 
-    public RabbitMQService(String host, int port) {
-        factory.setHost(host);
-        factory.setPort(port);
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+        this.messageHandler = messageHandler;
     }
-
 
     @Override
-    public void consumeAndPublish(String inputQueue, String outputQueue1, String outputQueue2) throws IOException, TimeoutException {
-        try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel();){
-
-            channel.queueDeclare(inputQueue, false, false, false, null);
-            System.out.println(" [*] Waiting to start data collection. To exit press CTRL+C");
-
-            // callback for received messages
-            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-                System.out.println(" [x] Received '" + new String(delivery.getBody(), "UTF-8") + "' at " + LocalDateTime.now() + " ms");
-                System.out.println(" [x] Starting data collection...");
-                String message; // data collection output
-                System.out.println(" [x] Data collection finished. Sending data to queue: " + outputQueue1);
-                publish(message, outputQueue1);
-
-
-            };
-
-
-            // start listening for messages
-            channel.basicConsume(inputQueue, true, deliverCallback, consumerTag -> { });
-        }
-
-
+    public void publish(String queueName, String message) throws IOException {
+        channel.queueDeclare(queueName, false, false, false, null);
+        channel.basicPublish("", queueName, new AMQP.BasicProperties.Builder().contentType(CONTENT_TYPE_JSON).build(),
+                message.getBytes());
+        System.out.println("Sent \""+ message + "\" to queue: " + queueName);
     }
 
-    private void publish(String message, String queueName) {
-        try (
-                Connection connection = factory.newConnection();
-                Channel channel = connection.createChannel()
-                ){
-            channel.queueDeclare(queueName, false, false, false, null);
-            System.out.println(" [x] Sending message: " + message + " to queue: " + queueName);
-            channel.basicPublish("", queueName, null, message.getBytes());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    @Override
+    public void consume(String queueName) throws IOException {
+        channel.queueDeclare(queueName, false, false, false, null);
+
+        // Create a consumer to handle incoming messages
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println("Received message from queue " + queueName + ": " + message);
+                // Delegate message handling to the message handler
+                try {
+                    messageHandler.handleMessage(message);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        // Start consuming messages from the queue
+        channel.basicConsume(queueName, true, consumer);
+    }
+
+    @Override
+    public void close() throws IOException, TimeoutException {
+        channel.close();
+        connection.close();
     }
 }
