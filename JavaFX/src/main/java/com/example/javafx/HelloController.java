efcea80069917c54d29d4a027a06be8781942565
Martin <martin@frischmann.ch>
2023-06-14 02:24:26 +0200

    Created UI-Formular

--- JavaFX/src/main/java/com/example/javafx/HelloController.java
+++ JavaFX/src/main/java/com/example/javafx/HelloController.java
@@ -1,14 +1,30 @@
 package com.example.javafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button close_Button;
+
+    @FXML
+    void close_Application() {
+        Stage stage = (Stage) close_Button.getScene().getWindow();
+        stage.close();
     }
+
+
 }
+
+
