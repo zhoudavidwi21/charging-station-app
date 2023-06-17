47041187ed5a0660c3fd5ca248ffc271c584e436
David <david281100@hotmail.com>
2023-06-17 16:35:10 +0200

    added backend.bat which starts all backend services

---
+++ backend.bat
@@ -0,0 +1,5 @@
+start "Data Collection Dispatcher" java -jar ./Apps/Data_Collection_Dispatcher.jar
+start "Data Collection Receiver" java -jar ./Apps/Data_Collection_Receiver.jar
+start "Station Data Collector" java -jar ./Apps/Station_Data_Collector.jar
+start "PDF Generator" java -jar ./Apps/PDF_Generator.jar
+pause
